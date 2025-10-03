b5df17d1436bd66a034471f4abf1603eff585154
Zoltan Pekic <zpekic@hotmail.com>
2025-10-02 22:25:32 -0700

    Fixing the UCF problem
    UCF file was not being compiled (and stupid ISE never threw an error) and nothing was working. Now basic circuit shows some life, but the frequencies seems still off.

--- src/BaSys2/sys_microbasic_basys2.vhd
+++ src/BaSys2/sys_microbasic_basys2.vhd
@@ -20,10 +20,10 @@ use work.microbasic_package.all;
 
 entity sys_microbasic_basys2 is
     Port ( 
-				-- 50MHz on the Basys2 board
+				-- Internal clock: 50MHz on the Basys2 board
 				MCLK: in std_logic;
 				
-				-- 48MHz external clock
+				-- External clock: 48MHz "half-size" crystal on breadboard (ESC-220BX)
 				UCLK: in std_logic;
 				
 				-- Switches on baseboard
@@ -69,36 +69,55 @@ end sys_microbasic_basys2;
 
 architecture Structural of sys_microbasic_basys2 is
 
-
--- Connect to PmodUSBUART 
+-- Connect to PmodUSBUART 0 (main TDX/RXD)
 -- https://digilent.com/reference/pmod/pmodusbuart/reference-manual
 alias JA_RTS: std_logic is PIO(72); --JA1;
 alias JA_RXD: std_logic is PIO(73); --JA2;
 alias JA_TXD: std_logic is PIO(74); --JA3;
 alias JA_CTS: std_logic is PIO(75); --JA4;
 
--- Test points on JB
-alias JB_TEST1: std_logic is PIO(76);
-alias JB_TEST2: std_logic is PIO(77);
-alias JB_TEST3: std_logic is PIO(78);
-alias JB_TEST4: std_logic is PIO(79);
+-- Connect to PmodUSBUART 1 (debug TDX/RXD)
+-- https://digilent.com/reference/pmod/pmodusbuart/reference-manual
+alias JB_RTS: std_logic is PIO(76);
+alias JB_RXD: std_logic is PIO(77);
+alias JB_TXD: std_logic is PIO(78);
+alias JB_CTS: std_logic is PIO(79);
+
+-- unused JC
+alias JC1: std_logic is PIO(80);
+alias JC2: std_logic is PIO(81);
+alias JC3: std_logic is PIO(82);
+alias JC4: std_logic is PIO(83);
+
+-- test points on JD
+alias JD1: std_logic is PIO(84);
+alias JD2: std_logic is PIO(85);
+alias JD3: std_logic is PIO(86);
+alias JD4: std_logic is PIO(87);
 
 alias RESET: std_logic is BTN(3);
 
 -- debug
-signal test_static, test_clk: std_logic;
-signal digsel: std_logic_vector(2 downto 0);
 signal T, debug, freqcnt_value: std_logic_vector(31 downto 0);
 signal hexdata, showdigit: std_logic_vector(3 downto 0);
 signal freqcnt_in: std_logic;
 
-signal cnt50MHz: std_logic_vector(3 downto 0);
---alias vgaclk: std_logic is cnt50MHz(0);
-signal vgaclk: std_logic;
-
 signal prescale_baud, prescale_power: integer range 0 to 65535;
-signal freq307200, freq153600, freq76800, freq38400, freq19200, freq9600, freq4800, freq2400, freq1200, freq600, freq300: std_logic;		
-signal freq4096, freq32, freq8, freq4, freq2, hexclk: std_logic;		
+
+signal cnt50MHz: std_logic_vector(11 downto 0); -- 12 bit counter driven by 50MHz
+signal cnt307200: std_logic_vector(11 downto 0); -- 12 bit counter driven by 307.2kHz
+alias freq38400: std_logic is cnt307200(3);
+alias freq19200: std_logic is cnt307200(4);
+alias freq9600: std_logic is cnt307200(5);
+alias freq4800: std_logic is cnt307200(6); 
+alias freq2400: std_logic is cnt307200(7); 
+
+signal cnt4096: std_logic_vector(11 downto 0); -- 12 bit counter driven by 4.096kHz
+alias freq2048: std_logic is cnt4096(1); 
+alias freq32: std_logic is cnt4096(7); 
+alias freq8: std_logic is cnt4096(9);
+alias freq4: std_logic is cnt4096(10); 
+alias freq2: std_logic is cnt4096(11); 
 
 -- output path for Intel hex format
 signal TXD_READY: std_logic;
@@ -109,15 +128,11 @@ signal RXD_READY: std_logic;
 signal RXD_CHAR: std_logic_vector(7 downto 0);
 --signal txd_send: std_logic;
 
-
 ---
 signal switch: std_logic_vector(7 downto 0);
-alias sw_mode: std_logic is switch(0);
 alias sw_baudrate: std_logic_vector(2 downto 0) is switch(7 downto 5);
 -- 
 signal button: std_logic_vector(7 downto 0);
-alias btn_hexout: std_logic is button(0);
-alias btn_scroll: std_logic_vector(1 downto 0) is button(2 downto 1);
 
 ---- UART
 signal baudrate_x1, baudrate_x2, baudrate_x4: std_logic;
@@ -127,78 +142,47 @@ begin
 -- not used, inactive
 PS2C <= '1';
 PS2D <= '1';
+HSYNC <= '0';
+VSYNC <= '0';
+OUTRED <= "000";
+OUTGREEN <= "000";
+OUTBLUE <= "00";
 
--- divide external clock
-clock_ext: entity work.sn74hc4040 port map (
-			clock_10 => UCLK,	-- 48MHz "half-size" crystal on breadboard (ESC-220BX)
-			reset_11 => RESET,
-			q1_9 => vgaclk, 		-- 24MHz
-			q2_7 => open,			-- 12 (internal dotclk)
-			q3_6 => open,			-- 6
-			q4_5 => open,			-- 3
-			q5_3 => open, 			-- 1.5
-			q6_2 => open, 	 		-- 0.75
-			q7_4 =>   open,		-- 0.0375
-			q8_13 =>  open,		-- 
-			q9_12 =>  digsel(0),	-- 
-			q10_14 => digsel(1),	-- 
-			q11_15 => digsel(2),	-- 
-			q12_1 =>  open			-- 
-		);
+-- test the freq generator
+JD1 <= cnt4096(0);
+JD2 <= baudrate_x1;
+JD3 <= baudrate_x2;
+JD4 <= baudrate_x4;
+
+LED <= switch;
 
 -- divide internal clock   	
-clock_int: process(MCLK, freq153600, freq4096)
+clock_int: process(MCLK, cnt307200, cnt4096)
 begin
-	if (rising_edge(MCLK)) then
-		cnt50MHz <= std_logic_vector(unsigned(cnt50MHz) + 1);
-		if (prescale_baud = 0) then
-			freq307200 <= not freq307200;
-			prescale_baud <= (25000000 / (2 * 153600));
-		else
-			prescale_baud <= prescale_baud - 1;
-		end if;
-		if (prescale_power = 0) then
-			freq4096 <= not freq4096;
-			prescale_power <= (25000000 / (2 * 4096));
-		else
-			prescale_power <= prescale_power - 1;
+	if (RESET = '1') then
+		prescale_baud <= 0;
+		prescale_power <= 0;
+		cnt307200 <= (others => '0');
+		cnt4096 <= (others => '0');
+	else
+		if (rising_edge(MCLK)) then
+			cnt50MHz <= std_logic_vector(unsigned(cnt50MHz) + 1);
+			if (prescale_baud = 0) then
+				cnt307200 <= std_logic_vector(unsigned(cnt307200) + 1);
+				prescale_baud <= (25000000 / (2 * 153600));
+			else
+				prescale_baud <= prescale_baud - 1;
+			end if;
+			if (prescale_power = 0) then
+				cnt4096 <= std_logic_vector(unsigned(cnt4096) + 1);
+				prescale_power <= (25000000 / (2 * 4096));
+			else
+				prescale_power <= prescale_power - 1;
+			end if;
 		end if;
 	end if;
 end process;
 
-baudgen: entity work.sn74hc4040 port map (
-			clock_10 => freq307200,
-			reset_11 => RESET,
-			q1_9 => freq153600, 
-			q2_7 => freq76800,
-			q3_6 => freq38400,		
-			q4_5 => freq19200,		
-			q5_3 => freq9600,		
-			q6_2 => freq4800, 	
-			q7_4 => freq2400,		
-			q8_13 => freq1200,		
-			q9_12 =>  freq600,
-			q10_14 => freq300,	
-			q11_15 => open,	
-			q12_1 =>  open	
-		);
---
-powergen: entity work.sn74hc4040 port map (
-			clock_10 => freq4096,
-			reset_11 => RESET,
-			q1_9 => open, 
-			q2_7 => open,
-			q3_6 => open,		
-			q4_5 => open,		
-			q5_3 => open,		
-			q6_2 => open, 	
-			q7_4 => open,		
-			q8_13 => freq32,		
-			q9_12 =>  open,	
-			q10_14 => freq8,	
-			q11_15 => freq4,	
-			q12_1 =>  freq2	
-		);
 --	
 	debounce_sw: entity work.debouncer8channel Port map ( 
 		clock => freq19200, 
@@ -215,21 +199,20 @@ powergen: entity work.sn74hc4040 port map (
 		signal_debounced => button
 	);
 	
-
 -- display some debug data of 6-digit 7-seg display	
 leds: entity work.fourdigitsevensegled port map ( 
 			  -- inputs
-			  data => debug(15 downto 0),
-			  digsel => digSel(1 downto 0),
+			  data => T(15 downto 0),
+			  digsel => cnt4096(7 downto 6),
            showdigit => "0000",	-- all digits on
-			  showdot => "1011",		-- middle digit on
+			  showdot => "1111",		-- no dots
 			  -- outputs
            anode => AN,
 			  segment(7) => DP,
 			  segment(6 downto 0) => SEG
 			 );	 
 
-
+-- simple character echo / loopback
 on_rxd_ready: process(RXD_READY, RXD_CHAR, reset)
 begin
 	if (reset = '1') then
@@ -240,18 +223,6 @@ begin
 		end if;
 	end if;
 end process;
-
--- count signal frequencies
-freqcnt: entity work.freqcounter Port map ( 
-		reset => RESET,
-      clk => freq2,
-      freq => freqcnt_in,
-		bcd => '1',
-		add => X"00000004",
-		cin => '0',
-		cout => open,
-      value => freqcnt_value
-	);
 	
 -- UART connection to the host
 txdout: entity work.uart_par2ser Port map (
@@ -274,34 +245,21 @@ rxdinp: entity work.uart_ser2par Port map (
          rxd => JA_TXD
 		);
 		
-with sw_baudrate select
-		baudrate_x4 <= freq153600 when "111",
-							freq76800 when "110", 
-							freq38400 when "101",
-							freq19200 when "100",		
-							freq9600 when "011",		
-							freq4800 when "010",		
-							freq2400 when "001", 	
-							freq1200 when others;		
-
-with sw_baudrate select
-		baudrate_x2 <= freq76800 when "111", 
-							freq38400 when "110",
-							freq19200 when "101",		
-							freq9600 when "100",		
-							freq4800 when "011",		
-							freq2400 when "010", 	
-							freq1200 when "001",
-						   freq600 when others;
-
-with sw_baudrate select
-		baudrate_x1 <= freq38400 when "111",
-							freq19200 when "110",		
-							freq9600 when "101",		
-							freq4800 when "100",		
-							freq2400 when "011", 
-							freq1200 when "010",
-							freq600  when "001",
-							freq300 when others;		
+-- UART baudrate selection
+baudrate_x1 <= cnt307200(to_integer(10 - unsigned(sw_baudrate)));
+baudrate_x2 <= cnt307200(to_integer(9 - unsigned(sw_baudrate)));
+baudrate_x4 <= cnt307200(to_integer(8 - unsigned(sw_baudrate)));
+							
+-- count signal frequencies
+freqcnt: entity work.freqcounter Port map ( 
+		reset => RESET,
+		clk => cnt4096(1),
+		freq => baudrate_x1,
+		bcd => '1',
+		add => X"00000004",
+		cin => '0',
+		cout => open,
+		value => freqcnt_value
+	);							
 end;
 
